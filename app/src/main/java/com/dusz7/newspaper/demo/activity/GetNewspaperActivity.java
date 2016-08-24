48313ef0f94f98865ae3bdb1c45e76e2a8fafa8e
dusz7 <dusz2599@163.com>
2016-08-24 12:33:25 +0800

    目前还差地理位置信息和领取结果展示

--- app/src/main/java/com/dusz7/newspaper/demo/activity/GetNewspaperActivity.java
+++ app/src/main/java/com/dusz7/newspaper/demo/activity/GetNewspaperActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.PhoneStateListener;
@@ -21,13 +23,14 @@ import org.json.JSONObject;
 /**
  * Created by dusz2 on 2016/8/16 0016.
  */
-public class GetNewspaperActivity extends AppCompatActivity implements Runnable {
+public class GetNewspaperActivity extends AppCompatActivity {
 
     private EditText phoneEditText;
 
     private String myPhone;
 
-    private String newspaperInformation;
+    final int RESULT_CODE = 11;
+//    private String newspaperInformation;
 
     private boolean isRegister = false;
 
@@ -38,73 +41,113 @@ public class GetNewspaperActivity extends AppCompatActivity implements Runnable 
         setContentView(R.layout.activity_get_newspaper);
 
         phoneEditText = (EditText)findViewById(R.id.phone_edit_text);
-        myPhone = phoneEditText.getText().toString();
+
 
         MyPhoneStateListener phoneListener = new MyPhoneStateListener(); //我们派生的类
         TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
         telephonyManager.listen(phoneListener, PhoneStateListener.LISTEN_CALL_STATE);
 
-        Intent intent = this.getIntent();
-        this.newspaperInformation = intent.getStringExtra("newpaper");
+//        Intent intent = this.getIntent();
+//        this.newspaperInformation = intent.getStringExtra("newspaper");
     }
 
     public void verification_getting_onClick(View v){
+        myPhone = phoneEditText.getText().toString();
 
-        Thread verificationThread = new Thread(GetNewspaperActivity.this);
+
+        Thread verificationThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //线程执行内容
+
+                //判断是否注册
+
+                String url = "";
+                InternetUtil internetUtil = new InternetUtil(url);
+                try{
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("phone",myPhone);
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+                //internetUtil.putMethod(jsonObject);
+
+
+                //涉及返回结果
+                Message msg = new Message();
+                if(isRegister){
+                    msg.what = 0;
+                }else {
+                    msg.what = 1;
+                }
+
+                handler.sendMessage(msg);
+
+            }
+        });
+        //开启线程
         verificationThread.start();
     }
 
-    @Override
-    public void run(){
 
-        String url = "";
-        InternetUtil internetUtil = new InternetUtil(url);
-        try{
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("newspaper",this.newspaperInformation);
-            jsonObject.put("phone",this.myPhone);
-        }catch (JSONException e){
-            e.printStackTrace();
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            super.handleMessage(msg);
+            switch (msg.what){
+                case 0:
+                    Intent intent = new Intent();
+                    intent.putExtra("phone",myPhone);
+                    intent.putExtra("isLogin",true);
+                    setResult(RESULT_CODE,intent);
+                    finish();
+                    //判断结果
+//                    String gettingResult = "Getting sucessfully!!";
+//                    intent.putExtra("gettingResult",gettingResult);
+
+//                    startActivity(intent);
+                    break;
+                case 1:
+                    AlertDialog.Builder builder = new AlertDialog.Builder(GetNewspaperActivity.this);
+
+                    builder.setTitle("用户未注册");
+                    builder.setMessage("如需继续操作，请为用户注册：\n"+myPhone);
+
+                    builder.setPositiveButton("确定注册", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //注册
+
+                            isRegister = true;
+
+                            Thread registerThread = new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    //线程执行内容
+                                }
+                            });
+                            //开启线程
+                            registerThread.start();
+
+                        }
+                    });
+
+                    builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
+                    builder.setCancelable(true);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    break;
+            }
+
         }
-        //internetUtil.putMethod(jsonObject);
-
-//        isRegister = false;
-        if(isRegister){
-
-            Intent intent = new Intent(GetNewspaperActivity.this,GettingResultActivity.class);
-            //判断结果
-            String gettingResult = "Getting sucessfully!!";
-            intent.putExtra("gettingResult",gettingResult);
-
-            startActivity(intent);
-
-        }else {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-            builder.setTitle("用户未注册");
-            builder.setMessage("是否要为手机用户："+this.myPhone+"进行注册");
-            builder.setPositiveButton("确定注册", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    //注册
-
-                    isRegister = true;
-                }
-            });
-
-            builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
-            });
-
-            builder.setCancelable(true);
-            AlertDialog dialog = builder.create();
-            dialog.show();
-        }
-
-    }
+    };
 
 
 
@@ -116,8 +159,8 @@ public class GetNewspaperActivity extends AppCompatActivity implements Runnable 
         public void onCallStateChanged(int state,String incomingNumber){
             Log.e("PhoneCallState", "Incoming number "+incomingNumber); //incomingNumber就是来电号码
 
-            myPhone = incomingNumber;
-            phoneEditText.setText(myPhone);
+//            myPhone = incomingNumber;
+            phoneEditText.setText(incomingNumber);
         }
 
     }
--- app/src/main/java/com/dusz7/newspaper/demo/activity/NewspaperInfoActivity.java
+++ app/src/main/java/com/dusz7/newspaper/demo/activity/NewspaperInfoActivity.java
@@ -1,10 +1,16 @@
 package com.dusz7.newspaper.demo.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dusz7.newspaper.demo.R;
 import com.dusz7.newspaper.demo.infoSaved.MyInternalStorage;
@@ -24,6 +30,17 @@ public class NewspaperInfoActivity extends AppCompatActivity {
     private TextView dateText;
     private TextView issueText;
     private TextView totalIssueText;
+
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
+
+    private boolean isLogin = false;
+
+    private String myPhone;
+
+    final int REQUEST_CODE = 1;
+    final int RESULT_CODE = 11;
 
 
     @Override
@@ -45,10 +62,6 @@ public class NewspaperInfoActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-
-        //Intent intent = this.getIntent();
-        //decodeResult = intent.getStringExtra("decodeResult");
-
         myNewspaper = new Newspaper(decodeResult);
 
         nameText.setText(myNewspaper.getName());
@@ -60,9 +73,42 @@ public class NewspaperInfoActivity extends AppCompatActivity {
 
     public void confirm_getting_onClick(View v){
 
-        myNewspaper.saveNewspaperInformation();
-        Intent intent = new Intent(NewspaperInfoActivity.this,GetNewspaperActivity.class);
-        intent.putExtra("newspaper",this.decodeResult);
-        startActivity(intent);
+        if (isLogin){
+            Toast.makeText(NewspaperInfoActivity.this,"已登录，查看结果",Toast.LENGTH_SHORT).show();
+            Thread gettingThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+                    //线程执行内容
+                    //发送手机号、报纸内容、地理位置
+                    //得到返回值：是否已领取，领取历史记录
+                }
+            });
+            //开启线程
+            gettingThread.start();
+
+            Intent intent = new Intent(NewspaperInfoActivity.this,GettingResultActivity.class);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(NewspaperInfoActivity.this,"尚未登录，请登录",Toast.LENGTH_SHORT).show();
+//            myNewspaper.saveNewspaperInformation();
+            Intent intent = new Intent(NewspaperInfoActivity.this,GetNewspaperActivity.class);
+            startActivityForResult(intent,REQUEST_CODE);
+        }
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode == RESULT_CODE && requestCode == REQUEST_CODE){
+            myPhone = data.getStringExtra("phone");
+            isLogin = data.getBooleanExtra("isLogin",false);
+
+        }
+    }
+
+
 }
